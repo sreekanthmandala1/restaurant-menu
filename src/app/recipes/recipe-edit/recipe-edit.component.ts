bfe01bac606330b936fce2239d69e34d32d2227e
sreekanth <sreekanthmandalaa@gmail.com>
2020-08-23 20:44:52 +0530

    frontend completed

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, FormArray, Validators } from '@angular/forms';
 import { RecipeService } from '../recipe.service';
-import { Recipe } from '../recipe.model';
+
 
 @Component({
   selector: 'app-recipe-edit',
@@ -13,7 +13,8 @@ export class RecipeEditComponent implements OnInit {
   id:number;
   editMode = false;
   recipeForm: FormGroup;
-  constructor(private route:ActivatedRoute, private recipeService:RecipeService) { }
+
+  constructor(private route:ActivatedRoute, private recipeService:RecipeService, private router: Router) { }
 
   ngOnInit(){
     this.route.params.subscribe((params:Params)=>{
@@ -33,6 +34,7 @@ export class RecipeEditComponent implements OnInit {
     }else{
       this.recipeService.addRecipe(this.recipeForm.value);
     }
+    this.onCancel();
   }
   onAddIngredient(){
     (<FormArray>this.recipeForm.get('ingredients')).push(
@@ -42,6 +44,12 @@ export class RecipeEditComponent implements OnInit {
       ])
       })
     );
+  }
+  onDeleteIngredient(index:number){
+    (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+  }
+  onCancel(){
+    this.router.navigate(["../"],{relativeTo : this.route});
   }
   private initForm(){
     let recipeName='';
